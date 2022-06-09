bca4a816064c0abb5d90de792f17365424967197
africoiner <confidencenyirenda7@gmail.com>
2022-06-09 15:24:52 +0200

    fixed bugs

--- shopping-cart/main.js
+++ shopping-cart/main.js
@@ -2,33 +2,33 @@ let shop = document.getElementById('shop');
 
 let shopItemsData = [
     {
-    id: "jfhgbvnscs",
+        id: "shop001",
         name: "Casual Shirt",
         price: 45,
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-        img: "images/img-1.jpg",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
+        img: "images/img-1.jpg"
     },
     {
-        id: "ioytrhndcv",
-        name: "Office Shirt",
-        price: 100,
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-        img: "images/img-2.jpg",
+        id: "shop002",
+        name: "Shirt's & Tie",
+        price: 65,
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
+        img: "images/img-2.jpg"
     },
     {
-        id: "wuefbncxbsn",
+        id: "shop003",
         name: "T Shirt",
         price: 25,
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-        img: "images/img-3.jpg",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
+        img: "images/img-3.jpg"
     },
     {
-        id: "thyfhcbcv",
-        name: "Mens Suit",
-        price: 300,
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
-        img: "images/img-4.jpg",
-    },
+        id: "shop004",
+        name: "Men's Suit",
+        price: 130,
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
+        img: "images/img-4.jpg"
+    }
 ];
 
 let basket = []
