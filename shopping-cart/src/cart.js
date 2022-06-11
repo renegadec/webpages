b202c25999994d64d9ddad933fc33474a749e779
africoiner <confidencenyirenda7@gmail.com>
2022-06-12 00:40:55 +0200

    fixed bugs

--- shopping-cart/src/cart.js
+++ shopping-cart/src/cart.js
@@ -15,13 +15,14 @@ let generateCartItems = () => {
         return (ShoppingCart.innerHTML = basket.map((x) => {
             let {id, item} = x;
             let search = shopItemsData.find((y) => y.id === id) || []
+            let {img, name, price} = search;
             return `<div class="cart-item">
-                        <img width="100" src="${search.img}" alt="" />
+                        <img width="100" src="${img}" alt="" />
                         <div class="details">
                             <div class="title-price-x">
                                 <h4 class="title-price">
-                                    <p>${search.name}</p>
-                                    <p class="cart-item-price">$ ${search.price}</p>
+                                    <p>${name}</p>
+                                    <p class="cart-item-price">$ ${price}</p>
                                 </h4>
                                 <i onclick="removeItem(${id})" class="bi bi-x-lg"></i>
                             </div>
@@ -98,12 +99,14 @@ let removeItem = (id) => {
     basket = basket.filter((x) => x.id !== selectedItem.id);
     generateCartItems();
     TotalAmount();
+    calculation();
     localStorage.setItem("data", JSON.stringify(basket));
 }
 
 let clearCart = () => {
     basket = [];
     generateCartItems();
+    calculation();
     localStorage.setItem("data", JSON.stringify(basket));
 }
 
