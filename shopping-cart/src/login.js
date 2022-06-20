b7d99faf004174d1472b493b1ed6e2cb8ac86715
africoiner <confidencenyirenda7@gmail.com>
2022-06-20 22:44:14 +0200

    added sigup validation

--- shopping-cart/src/login.js
+++ shopping-cart/src/login.js
@@ -5,8 +5,12 @@ const reg = document.getElementById('register');
 const btn = document.getElementById('btn');
 
 const user_name = document.getElementById('name');
+const reg_name = document.getElementById('reg_name');
 const password = document.getElementById('password');
+const reg_password = document.getElementById('reg_password');
+const confirm_password = document.getElementById('confirm_password')
 const errorMessage = document.getElementById('error');
+const pass_match = document.getElementById('pass_match');
 
 let register = () => {
     log_in.style.left = "-400px";
@@ -19,6 +23,38 @@ let login = () => {
     reg.style.left = "450px";
     btn.style.left = "0";
 }
+
+// ! Sign UP
+
+reg.addEventListener('submit', (e) => {
+    let messages = [];
+
+    // if (reg_password.value !== confirm_password.value){
+    //     messages.push('Password doesnt match')
+    // }
+
+    if (reg_name.value === '' || reg_name.value == null) {
+        messages.push('Name is required')
+    }
+
+    if (reg_password.value.length <= 6) {
+        messages.push('Password must be longer than 6 characters')
+    }
+
+    if (reg_password.value.length > 20 ) {
+        messages.push('Password must be less than 20 characters')
+    }
+
+    if (reg_password.value === 'password') {
+        messages.push('You cannot use that as your password')
+    }
+
+    if (messages.length > 0) {
+        e.preventDefault()
+        errorMessage.innerText = messages.join(', ')
+    }
+})
+
 
 log_in.addEventListener('submit', (e) => {
     let messages = [];
@@ -41,7 +77,16 @@ log_in.addEventListener('submit', (e) => {
     if (messages.length > 0) {
         e.preventDefault()
         errorMessage.innerText = messages.join(', ')
-    }
-
-    
+    }   
 })
+
+var check = function() {
+    if(reg_password.value !== confirm_password.value) {
+        pass_match.style.color = 'red';
+        pass_match.innerHTML = 'not matching'
+    }
+    else {
+        pass_match.style.color = 'green';
+        pass_match.innerHTML = 'matching'
+    }
+}
